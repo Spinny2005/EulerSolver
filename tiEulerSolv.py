ba85b9c0eeeb7c3abbd9e8db77274c8180749213
Spencer Boggs <spencer_boggs@icloud.com>
2022-12-01 11:15:58 -0800

    Update to catch null values

--- tiEulerSolv.py
+++ tiEulerSolv.py
@@ -17,42 +17,57 @@ input("    Press [Enter] to Start")
 while True:
     print("")
     y = input("Enter a y value: ")
-    y = float(y)
+    try: float(y)
+    except ValueError: y = None;
+    if (y != None):
+        y = float(y)
     x = input("Enter a x value: ")
-    x = float(x)
+    try: float(x)
+    except ValueError: x = None;
+    if (x != None):
+        x = float(y)
     stepCount = input("Number of steps: ")
     stepSize = input("Step size: ")
     derivative = input('Enter dy/dx: ')
     try: eval(derivative)
-    except NameError: derivative = None
+    except SyntaxError: derivative = None
     if (derivative != None):
-        if (str(y)) and (str(x)) and (stepCount) and (stepSize) and (derivative):
-            x = float(x)
-            y = float(y)
-            print('')
-
-            for i in range(int(stepCount)):
-                m = eval(derivative)
-                print("---- Step " + str(i+1) + " ----")
-                print("Slope: " + str(m))
-
-                print("y - " + str(y) + " = " + str(m) + "(x - " + str(x) + ")")
-                initialEquation = (str(m) + ' * x + ' + str(eval('(-x*m)+y')))
-
-                x = x+float(stepSize)
-                y = eval(str(initialEquation))
-        
-                print('Equation ' + str(i + 1) + ': y = ' + str(initialEquation))
-                print("Y after " + str(i+1) + " step(s): " + str(y))
+        try: eval(derivative)
+        except NameError: derivative = None
+        if (derivative != None):
+            if (str(y)) and (str(x)) and (stepCount) and (stepSize) and (derivative):
+                x = float(x)
+                y = float(y)
                 print('')
-                if (int(i+1) < int(stepCount)):
-                    a = input("Press [Enter] to go to step " + str(i+2))
-                elif (int(i+1) == int(stepCount)):
-                    a = input("Press [Enter] to proceed")
-                print('')        
+
+                for i in range(int(stepCount)):
+                    m = eval(derivative)
+                    print("---- Step " + str(i+1) + " ----")
+                    print("Slope: " + str(m))
+
+                    print("y - " + str(y) + " = " + str(m) + "(x - " + str(x) + ")")
+                    initialEquation = (str(m) + ' * x + ' + str(eval('(-x*m)+y')))
+
+                    x = x+float(stepSize)
+                    y = eval(str(initialEquation))
+
+                    print('Equation ' + str(i + 1) + ': y = ' + str(initialEquation))
+                    print("Y after " + str(i+1) + " step(s): " + str(y))
+                    print('')
+                    if (int(i+1) < int(stepCount)):
+                        a = input("Press [Enter] to go to step " + str(i+2))
+                    elif (int(i+1) == int(stepCount)):
+                        a = input("Press [Enter] to proceed")
+                    print('')        
+            else:
+                print('Unsolvable. Double check')
+                print('your inputs.')
         else:
-            print('Unsolvable. Double check')
-            print('your inputs.')
+            print('Please enter a valid')
+            print('equation. Enter any')
+            print('multiplication with *')
+            print('Ex: cx -> c*x')
+            input("Press [Enter] to proceed")
     else:
         print('Please enter a valid')
         print('equation. Enter any')
