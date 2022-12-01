408ca3c53e2033a859762788d505aa5bd1cc0f72
Spencer Boggs <spencer_boggs@icloud.com>
2022-11-30 21:33:07 -0800

    Created TI Adapted Version

---
+++ tiEulerSolv.py
@@ -0,0 +1,50 @@
+# Created by Spencer Boggs
+# Version adapted for TI python calculators
+import math
+
+print("")
+print("       Welcome to Euler's")
+print("         Method Solver!")
+print("    Created by Spencer Boggs")
+print("")
+print("  Remember to use proper syntax")
+print("  Use parenthesis for complex")
+print("  fractions and fractional")
+print("  expressions.")
+print("")
+input("    Press [Enter] to Start")
+while True:
+    print("")
+    y = input("Enter a y value: ")
+    x = input("Enter a x value: ")
+    stepCount = input("Number of steps: ")
+    stepSize = input("Step size: ")
+    derivative = input('Enter dy/dx: ')
+
+    if (y) and (x) and (stepCount) and (stepSize) and (derivative):
+        x = float(x)
+        y = float(y)
+        print('')
+
+        for i in range(int(stepCount)):
+            m = eval(derivative)
+            print("---- Step " + str(i+1) + " ----")
+            print("Slope: " + str(m))
+
+            print("y - " + str(y) + " = " + str(m) + "(x - " + str(x) + ")")
+            initialEquation = (str(m) + ' * x + ' + str(eval('(-x*m)+y')))
+
+            x = x+float(stepSize)
+            y = eval(str(initialEquation))
+        
+            print('Tangent Line ' + str(i + 1) + ': y = ' + str(initialEquation))
+            print("Y after " + str(i+1) + " step(s): " + str(y))
+            print('')
+            if (int(i+1) < int(stepCount)):
+                a = input("Press [Enter] to go to step " + str(i+2))
+            elif (int(i+1) == int(stepCount)):
+                a = input("Press [Enter] to proceed")
+            print('')        
+    else:
+        print('Unsolvable. Double check')
+        print('your inputs.')
