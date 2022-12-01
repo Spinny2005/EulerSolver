76c685b5f06462dbb115cfe59ff96faa47f0c288
Spencer Boggs <spencer_boggs@icloud.com>
2022-11-30 21:01:02 -0800

    Fished Program

--- eulersolver.py
+++ eulersolver.py
@@ -1,5 +1,6 @@
 # Created by Spencer Boggs
 import math
+
 print("")
 print("       Welcome to Euler's")
 print("         Method Solver!")
@@ -14,6 +15,7 @@ print("")
 input("    Press [Enter] to Start")
 while True:
     print("")
+    
     y = input("Enter a y value: ")
     x = input("Enter a x value: ")
 
@@ -25,21 +27,21 @@ while True:
     elif (initialX == 'u'):
         stepSize = input("Step Size: ")
 
-    x = float(x)+float(stepSize)
+    x = float(x)
     y = float(y)
     derivative = input('Enter dy/dx: ')
-    
-    # TO DO
-    # solve dy/dx to find y
-    # test 
+    print('')
 
     for i in range(int(stepCount)):
         m = eval(derivative)
-        print(str(m))
-        # y-y0=m(x-x0)
-        # y=m(x-x0)+y0
-        initialEquation = (str(m) + ' * x + ' + str(eval('(x*m)+y')))
-        print('')
-        print('Tangent Equation ' + str(i + 1) + ': y = ' + str(initialEquation))
-        y = eval(str(initialEquation))
+        print("Slope: " + str(m))
+
+        print("y - " + str(y) + " = " + str(m) + "(x - " + str(x) + ")")
+        initialEquation = (str(m) + ' * x + ' + str(eval('(-x*m)+y')))
+
         x = x+float(stepSize)
+        y = eval(str(initialEquation))
+        
+        print('Tangent Equation ' + str(i + 1) + ': y = ' + str(initialEquation))
+        print("Y Coordinate after " + str(i+1) + " step(s): " + str(y))
+        print('')
