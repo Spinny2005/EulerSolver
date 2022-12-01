f0f28170cd9eb246e6e87d566cd0470d81f40e76
Spencer Boggs <spencer_boggs@icloud.com>
2022-11-30 21:30:39 -0800

    Small Updates

--- eulersolver.py
+++ eulersolver.py
@@ -10,38 +10,40 @@ print("  Remember to use proper syntax")
 print("  Use parenthesis for complex")
 print("  fractions and fractional")
 print("  expressions.")
-print("  Enter u for unknown values")
 print("")
 input("    Press [Enter] to Start")
 while True:
     print("")
-    
     y = input("Enter a y value: ")
     x = input("Enter a x value: ")
-
     stepCount = input("Enter the number of steps: ")
-    initialX = input("Enter initial x position: ")
-    if (initialX != 'u'):
-        finalX = input("Enter final x position: ")
-        stepSize = float(stepCount)/(float(finalX) - float(initialX))
-    elif (initialX == 'u'):
-        stepSize = input("Step Size: ")
-
-    x = float(x)
-    y = float(y)
+    stepSize = input("Step size: ")
     derivative = input('Enter dy/dx: ')
-    print('')
 
-    for i in range(int(stepCount)):
-        m = eval(derivative)
-        print("Slope: " + str(m))
-
-        print("y - " + str(y) + " = " + str(m) + "(x - " + str(x) + ")")
-        initialEquation = (str(m) + ' * x + ' + str(eval('(-x*m)+y')))
-
-        x = x+float(stepSize)
-        y = eval(str(initialEquation))
-        
-        print('Tangent Equation ' + str(i + 1) + ': y = ' + str(initialEquation))
-        print("Y Coordinate after " + str(i+1) + " step(s): " + str(y))
+    if (y) and (x) and (stepCount) and (stepSize) and (derivative):
+        x = float(x)
+        y = float(y)
         print('')
+
+        for i in range(int(stepCount)):
+            m = eval(derivative)
+            print("---- Step " + str(i+1) + " ----")
+            print("Slope: " + str(m))
+
+            print("y - " + str(y) + " = " + str(m) + "(x - " + str(x) + ")")
+            initialEquation = (str(m) + ' * x + ' + str(eval('(-x*m)+y')))
+
+            x = x+float(stepSize)
+            y = eval(str(initialEquation))
+        
+            print('Tangent Equation ' + str(i + 1) + ': y = ' + str(initialEquation))
+            print("Y Coordinate after " + str(i+1) + " step(s): " + str(y))
+            print('')
+            if (int(i+1) < int(stepCount)):
+                a = input("Press [Enter] to proceed to step " + str(i+2))
+            elif (int(i+1) == int(stepCount)):
+                a = input("Press [Enter] to proceed")
+            print('')        
+    else:
+        print('Unsolvable. Double check')
+        print('your inputs.')
